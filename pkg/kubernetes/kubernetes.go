b2752896b41d201857e185556f41d830d6a02475
Lior Kamrat <46730876+likamrat@users.noreply.github.com>
2019-08-02 14:13:29 -0700

    fix url (#26)

--- pkg/kubernetes/kubernetes.go
+++ pkg/kubernetes/kubernetes.go
@@ -2,7 +2,7 @@ package kubernetes
 
 import "os/exec"
 
-const actionsManifestPath = "https://raw.githubusercontent.com/actions-org/actions/master/deploy/actions.yaml?token=AD4KK473CQJX24LS2MENQBS5CQTRQ"
+const actionsManifestPath = "https://raw.githubusercontent.com/actionscore/actions/master/deploy/actions.yaml?token=ALEQ47FIM7Z7T5VOJMYXM5C5JXQGM"
 
 func Init() error {
 	err := runCmdAndWait("kubectl", "apply", "-f", actionsManifestPath)
