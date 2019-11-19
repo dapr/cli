543f48d3490661e6a0814988d52ce0b02baafb79
Phillip Hoff <phillip@orst.edu>
2019-11-19 12:34:19 -0800

    Allow Docker network be specified via environment variable.

--- cmd/init.go
+++ cmd/init.go
@@ -12,15 +12,18 @@ import (
 	"github.com/dapr/cli/pkg/print"
 	"github.com/dapr/cli/pkg/standalone"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var kubernetesMode bool
 var runtimeVersion string
-var dockerNetwork string
 
 var InitCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Setup dapr in Kubernetes or Standalone modes",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		viper.BindPFlag("network", cmd.Flags().Lookup("network"))
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		print.PendingStatusEvent(os.Stdout, "Making the jump to hyperspace...")
 
@@ -33,6 +36,7 @@ var InitCmd = &cobra.Command{
 			}
 			print.SuccessStatusEvent(os.Stdout, "Success! Dapr has been installed. To verify, run 'kubectl get pods -w' in your terminal")
 		} else {
+			dockerNetwork := viper.GetString("network")
 			standalone.Uninstall(true, dockerNetwork)
 			err := standalone.Init(runtimeVersion, dockerNetwork)
 			if err != nil {
@@ -47,7 +51,7 @@ var InitCmd = &cobra.Command{
 func init() {
 	InitCmd.Flags().BoolVar(&kubernetesMode, "kubernetes", false, "Deploy Dapr to a Kubernetes cluster")
 	InitCmd.Flags().StringVarP(&runtimeVersion, "runtime-version", "", "latest", "The version of the Dapr runtime to install. for example: v0.1.0-alpha")
-	InitCmd.Flags().StringVarP(&dockerNetwork, "network", "", "", "The Docker network on which to deploy the Dapr runtime")
+	InitCmd.Flags().String("network", "", "The Docker network on which to deploy the Dapr runtime")
 
 	RootCmd.AddCommand(InitCmd)
 }
