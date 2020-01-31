423dd65071d7083862736ca0e3546b6579e12504
Young Bu Park <youngp@microsoft.com>
2020-01-31 15:44:31 -0800

    Remove vendor

--- vendor/github.com/dapr/dapr/pkg/apis/configuration/v1alpha1/doc.go
+++
@@ -1,8 +0,0 @@
-// ------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-// ------------------------------------------------------------
-
-// +k8s:deepcopy-gen=package
-// +groupName=configuration.dapr.io
-package v1alpha1
