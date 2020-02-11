c97f07c17004745d36a7b77a61147535ce87687b
yaron2 <yaronsc@microsoft.com>
2020-02-10 18:13:09 -0800

    add license

--- pkg/standalone/mtls_config.go
+++ pkg/standalone/mtls_config.go
@@ -1,4 +1,10 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
 package standalone
+
 type mtlsConfig struct {
 	Spec struct {
 		MTLS struct {
