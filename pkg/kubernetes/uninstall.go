433c9c8f67d750365bf949b5f3122003128f38d2
yaron2 <yaronsc@microsoft.com>
2020-03-10 12:20:36 -0700

    disable k8s uninstall error

--- pkg/kubernetes/uninstall.go
+++ pkg/kubernetes/uninstall.go
@@ -6,16 +6,12 @@
 package kubernetes
 
 import (
-	"errors"
-
 	"github.com/dapr/cli/utils"
 )
 
 // Uninstall removes Dapr
 func Uninstall() error {
-	_, err := utils.RunCmdAndWait("kubectl", "delete", "-f", daprManifestPath)
-	if err != nil {
-		return errors.New("is Dapr running? uninstall does not remove Dapr when installed via Helm")
-	}
+	//TODO: change how we handle init and uninstall for Kubernetes
+	utils.RunCmdAndWait("kubectl", "delete", "-f", daprManifestPath)
 	return nil
 }
