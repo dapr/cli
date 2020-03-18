157a87f1cd7584de3941c79a8ea82718064d2473
pruthvidhodda <60198385+pruthvidhodda@users.noreply.github.com>
2020-03-18 12:47:09 -0700

    Update the URL to pull dapr-operator.yaml (#293)
    * Update the URL to pull dapr-operator.yaml
    
    * Get manifest from latest release
    
    * Add nosec to avoid variable URL linter error
    
    * Move GetLatestRelease() to pkg/version
    
    * Move two consts to pkg/version
    
    * Change package alias

--- pkg/kubernetes/kubernetes.go
+++ pkg/kubernetes/kubernetes.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/dapr/cli/pkg/print"
+	cli_ver "github.com/dapr/cli/pkg/version"
 	"github.com/dapr/cli/utils"
 
 	"github.com/briandowns/spinner"
@@ -19,14 +20,19 @@ import (
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const daprManifestPath = "https://daprreleases.blob.core.windows.net/manifest/dapr-operator.yaml"
-
 // Init deploys the Dapr operator
 func Init() error {
 	client, err := DaprClient()
 	if err != nil {
 		return fmt.Errorf("can't connect to a Kubernetes cluster: %v", err)
 	}
+
+	version, err := cli_ver.GetLatestRelease(cli_ver.DaprGitHubOrg, cli_ver.DaprGitHubRepo)
+	if err != nil {
+		return fmt.Errorf("cannot get the manifest file: %s", err)
+	}
+
+	var daprManifestPath string = "https://github.com/dapr/dapr/releases/download/" + version + "/dapr-operator.yaml"
 
 	msg := "Deploying the Dapr Operator to your cluster..."
 	var s *spinner.Spinner
