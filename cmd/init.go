c5f5a9a802b369a95b1004b890e1a35c69612c5a
abhishek <abhirockzz@gmail.com>
2020-04-02 11:20:53 +0530

    updated help message

--- cmd/init.go
+++ cmd/init.go
@@ -54,7 +54,7 @@ var InitCmd = &cobra.Command{
 func init() {
 	InitCmd.Flags().BoolVarP(&kubernetesMode, "kubernetes", "k", false, "Deploy Dapr to a Kubernetes cluster")
 	InitCmd.Flags().StringVarP(&runtimeVersion, "runtime-version", "", "latest", "The version of the Dapr runtime to install. for example: v0.1.0-alpha")
-	InitCmd.Flags().StringVarP(&kubernetesNamespace, "namespace", "", "default", "(Optional) Existing Kubernetes namespace for Dapr installation. The 'default' namespace is used if not specified")
+	InitCmd.Flags().StringVarP(&kubernetesNamespace, "namespace", "", "default", "(Optional) Existing Kubernetes namespace for Dapr installation")
 	InitCmd.Flags().String("network", "", "The Docker network on which to deploy the Dapr runtime")
 	InitCmd.Flags().String("install-path", "", "The optional location to install Dapr to.  The default is /usr/local/bin for Linux/Mac and C:\\dapr for Windows")
 
