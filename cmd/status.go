8ce2e7755e5d6274c4fb15c994f08def3db6f37d
yaron2 <yaronsc@microsoft.com>
2020-04-06 14:22:56 -0700

    add dapr control plane status

---
+++ cmd/status.go
@@ -0,0 +1,41 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/dapr/cli/pkg/kubernetes"
+	"github.com/dapr/cli/pkg/print"
+	"github.com/dapr/cli/utils"
+	"github.com/gocarina/gocsv"
+	"github.com/spf13/cobra"
+)
+
+var StatusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Shows status and health of the Dapr control plane on Kubernetes",
+	Run: func(cmd *cobra.Command, args []string) {
+		status, err := kubernetes.Status()
+		if err != nil {
+			print.FailureStatusEvent(os.Stdout, err.Error())
+			os.Exit(1)
+		}
+		table, err := gocsv.MarshalString(status)
+		if err != nil {
+			print.FailureStatusEvent(os.Stdout, err.Error())
+			os.Exit(1)
+		}
+
+		utils.PrintTable(table)
+	},
+}
+
+func init() {
+	StatusCmd.Flags().BoolVarP(&k8s, "kubernetes", "k", true, "Only works with a Kubernetes cluster")
+	StatusCmd.MarkFlagRequired("kubernetes")
+	RootCmd.AddCommand(StatusCmd)
+}
---
+++ pkg/kubernetes/status.go
@@ -0,0 +1,98 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package kubernetes
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/dapr/cli/pkg/age"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+)
+
+var (
+	controlPlaneLabels = []string{"dapr-operator", "dapr-sentry", "dapr-placement", "dapr-sidecar-injector"}
+)
+
+// StatutOutput represents the status of a named Dapr resource.
+type StatusOutput struct {
+	Name      string `csv:"NAME"`
+	Namespace string `csv:"NAMESPACE"`
+	Healthy   string `csv:"HEALTHY"`
+	Status    string `csv:"STATUS"`
+	Version   string `csv:"VERSION"`
+	Age       string `csv:"AGE"`
+	Created   string `csv:"CREATED"`
+}
+
+// List status for Dapr resources.
+func Status() ([]StatusOutput, error) {
+	client, err := Client()
+	if err != nil {
+		return nil, err
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(controlPlaneLabels))
+
+	m := sync.Mutex{}
+	statuses := []StatusOutput{}
+
+	for _, lbl := range controlPlaneLabels {
+		go func(label string) {
+			p, err := client.CoreV1().Pods(v1.NamespaceAll).List(v1.ListOptions{
+				LabelSelector: labels.FormatLabels(
+					map[string]string{
+						"app": label,
+					},
+				),
+			})
+			if err == nil && len(p.Items) == 1 {
+				pod := p.Items[0]
+
+				image := pod.Spec.Containers[0].Image
+				namespace := pod.GetNamespace()
+				age := age.GetAge(pod.CreationTimestamp.Time)
+				created := pod.CreationTimestamp.Format("2006-01-02 15:04.05")
+				version := image[strings.IndexAny(image, ":")+1:]
+				status := ""
+
+				if pod.Status.ContainerStatuses[0].State.Waiting != nil {
+					status = fmt.Sprintf("Waiting (%s)", pod.Status.ContainerStatuses[0].State.Waiting.Reason)
+				} else if pod.Status.ContainerStatuses[0].State.Running != nil {
+					status = "Running"
+				} else if pod.Status.ContainerStatuses[0].State.Terminated != nil {
+					status = "Terminated"
+				}
+
+				healthy := "False"
+				if pod.Status.ContainerStatuses[0].Ready {
+					healthy = "True"
+				}
+
+				s := StatusOutput{
+					Name:      label,
+					Namespace: namespace,
+					Created:   created,
+					Age:       age,
+					Status:    status,
+					Version:   version,
+					Healthy:   healthy,
+				}
+
+				m.Lock()
+				statuses = append(statuses, s)
+				m.Unlock()
+			}
+			wg.Done()
+		}(lbl)
+	}
+
+	wg.Wait()
+	return statuses, nil
+}
