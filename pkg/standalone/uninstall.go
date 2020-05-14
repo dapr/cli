ac84b75c52332ba11fca6d2c03be84170f808ff0
Sashi Kumar <ksashikumark93@gmail.com>
2020-05-14 22:33:09 +0200

    Call DeleteRunDataFile even if docker is not running

--- pkg/standalone/uninstall.go
+++ pkg/standalone/uninstall.go
@@ -10,52 +10,60 @@ import (
 
 // Uninstall deletes all installed containers
 func Uninstall(uninstallAll bool, dockerNetwork string) error {
+	var containerErrs []error
+
 	dockerInstalled := utils.IsDockerInstalled()
 	if !dockerInstalled {
-		return errors.New("could not connect to Docker. Docker may not be installed or running.")
-	}
-
-	var errs []error
-
-	_, err := utils.RunCmdAndWait(
-		"docker", "rm",
-		"--force",
-		utils.CreateContainerName(DaprPlacementContainerName, dockerNetwork))
-
-	if err != nil {
-		errs = append(errs, fmt.Errorf("could not remove %s container: %s", DaprPlacementContainerName, err))
-	}
-
-	_, err = utils.RunCmdAndWait(
-		"docker", "rmi",
-		"--force",
-		daprDockerImageName)
-
-	if err != nil {
-		errs = append(errs, fmt.Errorf("could not remove %s container: %s", daprDockerImageName, err))
-	}
-
-	if uninstallAll {
-		_, err = utils.RunCmdAndWait(
+		containerErrs = append(
+			containerErrs,
+			fmt.Errorf("could not connect to Docker. Docker may not be installed or running"))
+	} else {
+		_, err := utils.RunCmdAndWait(
 			"docker", "rm",
 			"--force",
-			utils.CreateContainerName(DaprRedisContainerName, dockerNetwork))
+			utils.CreateContainerName(DaprPlacementContainerName, dockerNetwork))
+
 		if err != nil {
-			errs = append(errs, fmt.Errorf("could not remove %s container: %s", DaprRedisContainerName, err))
+			containerErrs = append(
+				containerErrs,
+				fmt.Errorf("could not remove %s container: %s", DaprPlacementContainerName, err))
+		}
+
+		_, err = utils.RunCmdAndWait(
+			"docker", "rmi",
+			"--force",
+			daprDockerImageName)
+
+		if err != nil {
+			containerErrs = append(
+				containerErrs,
+				fmt.Errorf("could not remove %s container: %s", daprDockerImageName, err))
+		}
+
+		if uninstallAll {
+			_, err = utils.RunCmdAndWait(
+				"docker", "rm",
+				"--force",
+				utils.CreateContainerName(DaprRedisContainerName, dockerNetwork))
+			if err != nil {
+				containerErrs = append(
+					containerErrs,
+					fmt.Errorf("could not remove %s container: %s", DaprRedisContainerName, err))
+			}
 		}
 	}
 
-	err = rundata.DeleteRunDataFile()
+	err := rundata.DeleteRunDataFile()
 	if err != nil {
 		fmt.Println("WARNING: could not delete run data file")
 	}
 
-	if len(errs) == 0 {
+	if len(containerErrs) == 0 {
 		return nil
 	}
 
 	err = errors.New("uninstall failed")
-	for _, e := range errs {
+	for _, e := range containerErrs {
 		err = fmt.Errorf("%w \n %s", err, e)
 	}
 	return err
