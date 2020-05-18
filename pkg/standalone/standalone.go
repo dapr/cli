6c89df56f72b9ce235721be8459a1c4762c5869f
Leon Mai <lemai@microsoft.com>
2020-05-18 14:50:49 -0700

    Download location is hardcoded while path to copy daprd to is not.  M… (#338)
    * Download location is hardcoded while path to copy daprd to is not.  Make them the same and configurable thru --install-path
    
    * conflict
    
    * Update comment
    
    Co-authored-by: LM <lemai>

--- pkg/standalone/standalone.go
+++ pkg/standalone/standalone.go
@@ -59,7 +59,9 @@ func isInstallationRequired(installLocation, requestedVersion string) bool {
 			destDir = daprDefaultLinuxAndMacInstallPath
 		}
 	}
-	daprdBinaryPath := path_filepath.Join(destDir, daprRuntimeFilePrefix) //e.g. /usr/local/bin/daprd or c:\\daprd
+
+	// e.g. /usr/local/bin/daprd or c:\dapr, which are the defaults unless overridden by "installLocation"
+	daprdBinaryPath := path_filepath.Join(destDir, daprRuntimeFilePrefix)
 
 	// first time install?
 	_, err := os.Stat(daprdBinaryPath)
@@ -111,7 +113,7 @@ func Init(runtimeVersion string, dockerNetwork string, installLocation string) e
 		return errors.New("could not connect to Docker. Docker may not be installed or running")
 	}
 
-	dir, err := getDaprDir()
+	downloadDest, err := getDownloadDest(installLocation)
 	if err != nil {
 		return err
 	}
@@ -137,7 +139,7 @@ func Init(runtimeVersion string, dockerNetwork string, installLocation string) e
 	}
 
 	for _, step := range initSteps {
-		go step(&wg, errorChan, dir, runtimeVersion, dockerNetwork, installLocation)
+		go step(&wg, errorChan, downloadDest, runtimeVersion, dockerNetwork, installLocation)
 	}
 
 	go func() {
@@ -166,11 +168,17 @@ func Init(runtimeVersion string, dockerNetwork string, installLocation string) e
 	return nil
 }
 
-func getDaprDir() (string, error) {
+func getDownloadDest(installLocation string) (string, error) {
 	p := ""
 
+	// use the install location passed in for Windows.  This can't
+	// be done for other environments because the install location default to a privileged dir: /usr/local/bin
 	if runtime.GOOS == daprWindowsOS {
-		p = path_filepath.FromSlash("c:/dapr")
+		if installLocation == "" {
+			p = daprDefaultWindowsInstallPath
+		} else {
+			p = installLocation
+		}
 	} else {
 		usr, err := user.Current()
 		if err != nil {
