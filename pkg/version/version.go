9474e448eb027f4bef12263d880620fada2a2721
willdavsmith <willdavsmith@gmail.com>
2020-08-05 13:56:19 -0400

    removed -rc tag downloads for dashboard

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -59,8 +59,7 @@ func GetLatestRelease(gitHubOrg, gitHubRepo string) (string, error) {
 	}
 
 	for _, release := range githubRepoReleases {
-		// Allow -rc tags for dashboard
-		if !strings.Contains(release.TagName, "-rc") || strings.Contains(releaseURL, "dashboard") {
+		if !strings.Contains(release.TagName, "-rc") {
 			return release.TagName, nil
 		}
 	}
