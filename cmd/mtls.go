24da635ab5574ac3650cf4f9ee53d8df2578808d
yaron2 <yaronsc@microsoft.com>
2020-08-12 16:40:39 -0700

    add certificate export command

--- cmd/mtls.go
+++ cmd/mtls.go
@@ -7,10 +7,15 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/dapr/cli/pkg/kubernetes"
+	"github.com/dapr/cli/pkg/print"
 	"github.com/spf13/cobra"
 )
+
+var exportPath string
 
 var MTLSCmd = &cobra.Command{
 	Use:   "mtls",
@@ -18,7 +23,7 @@ var MTLSCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		enabled, err := kubernetes.IsMTLSEnabled()
 		if err != nil {
-			fmt.Printf("error checking mTLS: %s \n", err)
+			print.FailureStatusEvent(os.Stdout, fmt.Sprintf("error checking mTLS: %s", err))
 			return
 		}
 
@@ -30,8 +35,25 @@ var MTLSCmd = &cobra.Command{
 	},
 }
 
+var ExportCMD = &cobra.Command{
+	Use:   "export",
+	Short: "Export the root CA, issuer cert and key from Kubernetes to local files",
+	Run: func(cmd *cobra.Command, args []string) {
+		err := kubernetes.ExportTrustChain(exportPath)
+		if err != nil {
+			print.FailureStatusEvent(os.Stdout, fmt.Sprintf("error exporting trust chain certs: %s", err))
+			return
+		}
+
+		dir, _ := filepath.Abs(exportPath)
+		print.SuccessStatusEvent(os.Stdout, fmt.Sprintf("Trust certs successfully exported to %s", dir))
+	},
+}
+
 func init() {
 	MTLSCmd.Flags().BoolVarP(&kubernetesMode, "kubernetes", "k", false, "Check if mTLS is enabled in a Kubernetes cluster")
+	ExportCMD.Flags().StringVarP(&exportPath, "out", "o", ".", "Output directory path to save the certs")
 	MTLSCmd.MarkFlagRequired("kubernetes")
+	MTLSCmd.AddCommand(ExportCMD)
 	RootCmd.AddCommand(MTLSCmd)
 }
--- pkg/kubernetes/mtls.go
+++ pkg/kubernetes/mtls.go
@@ -1,28 +1,92 @@
 package kubernetes
 
 import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/dapr/dapr/pkg/apis/configuration/v1alpha1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
-	defaultConfigName = "default"
+	systemConfigName      = "daprsystem"
+	trustBundleSecretName = "dapr-trust-bundle"
 )
 
 func IsMTLSEnabled() (bool, error) {
-	client, err := DaprClient()
+	c, err := getSystemConfig()
 	if err != nil {
 		return false, err
+	}
+	return c.Spec.MTLSSpec.Enabled, nil
+}
+
+func getSystemConfig() (*v1alpha1.Configuration, error) {
+	client, err := DaprClient()
+	if err != nil {
+		return nil, err
 	}
 
 	configs, err := client.ConfigurationV1alpha1().Configurations(meta_v1.NamespaceAll).List(meta_v1.ListOptions{})
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 
 	for _, c := range configs.Items {
-		if c.GetName() == defaultConfigName {
-			return c.Spec.MTLSSpec.Enabled, nil
+		if c.GetName() == systemConfigName {
+			return &c, nil
 		}
 	}
-	return false, nil
+	return nil, errors.New("system configuration not found")
+}
+
+func ExportTrustChain(outputDir string) error {
+	_, err := os.Stat(outputDir)
+
+	if os.IsNotExist(err) {
+		errDir := os.MkdirAll(outputDir, 0755)
+		if errDir != nil {
+			return err
+		}
+	}
+
+	_, client, err := GetKubeConfigClient()
+	if err != nil {
+		return err
+	}
+
+	c, err := getSystemConfig()
+	if err != nil {
+		return err
+	}
+	res, err := client.CoreV1().Secrets(c.GetNamespace()).List(meta_v1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, i := range res.Items {
+		if i.GetName() == trustBundleSecretName {
+			ca := i.Data["ca.crt"]
+			issuerCert := i.Data["issuer.crt"]
+			issuerKey := i.Data["issuer.key"]
+
+			err := ioutil.WriteFile(filepath.Join(outputDir, "ca.crt"), ca, 0700)
+			if err != nil {
+				return err
+			}
+
+			err = ioutil.WriteFile(filepath.Join(outputDir, "issuer.crt"), issuerCert, 0700)
+			if err != nil {
+				return err
+			}
+
+			err = ioutil.WriteFile(filepath.Join(outputDir, "issuer.key"), issuerKey, 0700)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
