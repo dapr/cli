e6e7acbb88783f2fdb6a769a4e3e5ae7ee98e313
yaron2 <yaronsc@microsoft.com>
2020-08-13 11:13:54 -0700

    refactor init on kubernetes to use Helm

--- cmd/init.go
+++ cmd/init.go
@@ -19,6 +19,9 @@ import (
 var kubernetesMode bool
 var slimMode bool
 var runtimeVersion string
+var initNamespace string
+var enableMTLS bool
+var enableHA bool
 
 var InitCmd = &cobra.Command{
 	Use:   "init",
@@ -33,13 +36,20 @@ var InitCmd = &cobra.Command{
 		print.PendingStatusEvent(os.Stdout, "Making the jump to hyperspace...")
 
 		if kubernetesMode {
-			print.InfoStatusEvent(os.Stdout, "Note: this installation is recommended for testing purposes. For production environments, please use Helm \n")
-			err := kubernetes.Init(fmt.Sprintf("v%s", runtimeVersion))
+			print.InfoStatusEvent(os.Stdout, "Note: To install Dapr using Helm, see here:  https://github.com/dapr/docs/blob/master/getting-started/environment-setup.md#using-helm-advanced\n")
+
+			config := kubernetes.InitConfiguration{
+				Namespace:  initNamespace,
+				Version:    runtimeVersion,
+				EnableMTLS: enableMTLS,
+				EnableHA:   enableHA,
+			}
+			err := kubernetes.Init(config)
 			if err != nil {
 				print.FailureStatusEvent(os.Stdout, err.Error())
 				return
 			}
-			print.SuccessStatusEvent(os.Stdout, "Success! Dapr has been installed. To verify, run 'kubectl get pods -w' or 'dapr status -k' in your terminal. To get started, go here: https://aka.ms/dapr-getting-started")
+			print.SuccessStatusEvent(os.Stdout, fmt.Sprintf("Success! Dapr has been installed to namespace %s. To verify, run `dapr status -k' in your terminal. To get started, go here: https://aka.ms/dapr-getting-started", config.Namespace))
 		} else {
 			dockerNetwork := ""
 			if !slimMode {
@@ -60,6 +70,9 @@ func init() {
 	InitCmd.Flags().BoolVarP(&kubernetesMode, "kubernetes", "k", false, "Deploy Dapr to a Kubernetes cluster")
 	InitCmd.Flags().BoolVarP(&slimMode, "slim", "s", false, "Initialize dapr in self-hosted mode without placement, redis and zipkin containers.")
 	InitCmd.Flags().StringVarP(&runtimeVersion, "runtime-version", "", "latest", "The version of the Dapr runtime to install. for example: v0.1.0")
+	InitCmd.Flags().StringVarP(&initNamespace, "namespace", "n", "dapr-system", "The Kubernetes namespace to install Dapr in")
+	InitCmd.Flags().BoolVarP(&enableMTLS, "enable-mtls", "", true, "Enable mTLS in your cluster. Default: true")
+	InitCmd.Flags().BoolVarP(&enableHA, "enable-ha", "", false, "Deploy Dapr in a highly available mode. Default: false")
 	InitCmd.Flags().String("network", "", "The Docker network on which to deploy the Dapr runtime")
 	InitCmd.Flags().String("redis-host", "localhost", "The host on which the Redis service resides")
 
--- pkg/kubernetes/kubernetes.go
+++ pkg/kubernetes/kubernetes.go
@@ -7,42 +7,40 @@ package kubernetes
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"runtime"
 	"time"
 
 	"github.com/dapr/cli/pkg/print"
-	cli_ver "github.com/dapr/cli/pkg/version"
-	"github.com/dapr/cli/utils"
+	v1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/helm/pkg/strvals"
 
 	"github.com/briandowns/spinner"
-	scheme "github.com/dapr/dapr/pkg/client/clientset/versioned"
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	helm "helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/cli"
 )
 
 const (
+	daprReleaseName   = "dapr"
+	daprHelmRepo      = "https://daprio.azurecr.io/helm/v1/repo"
 	daprLatestVersion = "latest"
 )
 
+type InitConfiguration struct {
+	Version    string
+	Namespace  string
+	EnableMTLS bool
+	EnableHA   bool
+}
+
 // Init deploys the Dapr operator using the supplied runtime version.
-func Init(version string) error {
-	client, err := DaprClient()
-	if err != nil {
-		return fmt.Errorf("can't connect to a Kubernetes cluster: %v", err)
-	}
-
-	if version == daprLatestVersion {
-		var v string
-		v, err = cli_ver.GetLatestRelease(cli_ver.DaprGitHubOrg, cli_ver.DaprGitHubRepo)
-		if err != nil {
-			return fmt.Errorf("cannot get the manifest file: %s", err)
-		}
-
-		version = v
-	}
-
-	var daprManifestPath string = "https://github.com/dapr/dapr/releases/download/" + version + "/dapr-operator.yaml"
-
+func Init(config InitConfiguration) error {
 	msg := "Deploying the Dapr control plane to your cluster..."
 	var s *spinner.Spinner
 
@@ -56,15 +54,7 @@ func Init(version string) error {
 		s.Start()
 	}
 
-	_, err = utils.RunCmdAndWait("kubectl", "apply", "-f", daprManifestPath)
-	if err != nil {
-		if s != nil {
-			s.Stop()
-		}
-		return err
-	}
-
-	err = installConfig(client)
+	err := install(config)
 	if err != nil {
 		if s != nil {
 			s.Stop()
@@ -79,9 +69,121 @@ func Init(version string) error {
 	return nil
 }
 
-// installConfig installs a configuration resource of a custom CRD called configurations.dapr.io
-func installConfig(client scheme.Interface) error {
-	config := GetDefaultConfiguration()
-	_, err := client.ConfigurationV1alpha1().Configurations(meta_v1.NamespaceDefault).Create(&config)
-	return err
+func createNamespace(namespace string) error {
+	_, client, err := GetKubeConfigClient()
+	if err != nil {
+		return fmt.Errorf("can't connect to a Kubernetes cluster: %v", err)
+	}
+
+	ns := &v1.Namespace{
+		ObjectMeta: meta_v1.ObjectMeta{
+			Name: namespace,
+		},
+	}
+	// try to create the namespace if it doesn't exist. ok to ignore error.
+	client.CoreV1().Namespaces().Create(ns)
+	return nil
+}
+
+func helmConfig(namespace string) (*helm.Configuration, error) {
+	ac := helm.Configuration{}
+	flags := &genericclioptions.ConfigFlags{
+		Namespace: &namespace,
+	}
+	err := ac.Init(flags, namespace, "secret", debugLog)
+	return &ac, err
+}
+
+func createTempDir() (string, error) {
+	dir, err := ioutil.TempDir("", "dapr")
+	if err != nil {
+		return "", fmt.Errorf("error creating temp dir: %s", err)
+	}
+	return dir, nil
+}
+
+func locateChartFile(dirPath string) (string, error) {
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(dirPath, files[0].Name()), nil
+}
+
+func daprChart(version string, config *helm.Configuration) (*chart.Chart, error) {
+	pull := helm.NewPull()
+	pull.RepoURL = daprHelmRepo
+	pull.Settings = &cli.EnvSettings{}
+
+	if version != daprLatestVersion {
+		pull.Version = chartVersion(version)
+	}
+
+	dir, err := createTempDir()
+	if err != nil {
+		return nil, err
+	}
+	defer os.RemoveAll(dir)
+
+	pull.DestDir = dir
+
+	_, err = pull.Run(daprReleaseName)
+	if err != nil {
+		return nil, err
+	}
+
+	chartPath, err := locateChartFile(dir)
+	if err != nil {
+		return nil, err
+	}
+	return loader.Load(chartPath)
+}
+
+func chartValues(config InitConfiguration) (map[string]interface{}, error) {
+	chartVals := map[string]interface{}{}
+	globalVals := []string{
+		fmt.Sprintf("global.ha.enabled=%t", config.EnableHA),
+		fmt.Sprintf("global.mtls.enabled=%t", config.EnableMTLS),
+	}
+
+	for _, v := range globalVals {
+		if err := strvals.ParseInto(v, chartVals); err != nil {
+			return nil, err
+		}
+	}
+	return chartVals, nil
+}
+
+func install(config InitConfiguration) error {
+	err := createNamespace(config.Namespace)
+	if err != nil {
+		return err
+	}
+
+	helmConf, err := helmConfig(config.Namespace)
+	if err != nil {
+		return err
+	}
+
+	daprChart, err := daprChart(config.Version, helmConf)
+	if err != nil {
+		return err
+	}
+
+	installClient := helm.NewInstall(helmConf)
+	installClient.ReleaseName = daprReleaseName
+	installClient.Namespace = config.Namespace
+
+	values, err := chartValues(config)
+	if err != nil {
+		return err
+	}
+
+	if _, err = installClient.Run(daprChart, values); err != nil {
+		return err
+	}
+	return nil
+}
+
+func debugLog(format string, v ...interface{}) {
 }
