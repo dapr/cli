c5b2e117759e9b7016109ec6fb00b6519c9320ad
Artur Souza <artursouza.ms@outlook.com>
2020-12-10 21:59:46 -0800

    Adds publish-app-id to publish command (#548)

--- pkg/standalone/client.go
+++ pkg/standalone/client.go
@@ -16,8 +16,8 @@ type daprProcess struct {
 type Client interface {
 	// Invoke is a command to invoke a remote or local dapr instance
 	Invoke(appID, method, data, verb string) (string, error)
-	// Publish is used to publish event to a topic in a pubsub.
-	Publish(topic, payload, pubsubName string) error
+	// Publish is used to publish event to a topic in a pubsub for an app ID.
+	Publish(publishAppID, pubsubName, topic, payload string) error
 }
 
 type Standalone struct {
