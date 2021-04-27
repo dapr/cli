4e41dd07501089fe0492ac85146e2c20092e6cd3
Phil Kedy <phil.kedy@gmail.com>
2021-04-27 17:01:36 -0400

    Authenticating to the Github API using the GITHUB_TOKEN environment variable, if set.
    Signed-off-by: Andy Le <anhldbk@gmail.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -33,7 +34,18 @@ type githubRepoReleaseItem struct {
 // GetLatestRelease return the latest release version of dapr.
 func GetLatestRelease(gitHubOrg, gitHubRepo string) (string, error) {
 	releaseURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases", gitHubOrg, gitHubRepo)
-	resp, err := http.Get(releaseURL)
+
+	req, err := http.NewRequest("GET", releaseURL, nil)
+	if err != nil {
+		return "", err
+	}
+
+	githubToken := os.Getenv("GITHUB_TOKEN")
+	if githubToken != "" {
+		req.Header.Add("Authorization", "token "+githubToken)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
