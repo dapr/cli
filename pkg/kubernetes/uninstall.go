91f1a186baa3b785bbad2acbeef876e0248398cd
yaron2 <yaronsc@microsoft.com>
2021-05-12 11:00:15 -0700

    remove crds on uninstall -k

--- pkg/kubernetes/uninstall.go
+++ pkg/kubernetes/uninstall.go
@@ -8,8 +8,15 @@ package kubernetes
 import (
 	"time"
 
+	"github.com/dapr/cli/utils"
 	helm "helm.sh/helm/v3/pkg/action"
 )
+
+var crdsFullResource = []string{
+	"configurations.dapr.io",
+	"components.dapr.io",
+	"subscriptions.dapr.io",
+}
 
 // Uninstall removes Dapr from a Kubernetes cluster.
 func Uninstall(namespace string, timeout uint) error {
@@ -21,5 +28,25 @@ func Uninstall(namespace string, timeout uint) error {
 	uninstallClient := helm.NewUninstall(config)
 	uninstallClient.Timeout = time.Duration(timeout) * time.Second
 	_, err = uninstallClient.Run(daprReleaseName)
+	if err != nil {
+		return err
+	}
+
+	err = removeCRDs()
+	if err != nil {
+		return err
+	}
+
 	return err
+}
+
+func removeCRDs() error {
+	for _, crd := range crdsFullResource {
+		_, err := utils.RunCmdAndWait("kubectl", "delete", "crd", crd)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
