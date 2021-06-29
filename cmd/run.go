0a37376658ef83e8a1b49178cf4e6f1c93eb1dfc
Dmitry Shmulevich <dmitry.shmulevich@gmail.com>
2021-06-29 09:22:15 -0700

    Allow to specify port in placement-host-address (#746)
    * Allow to specify port in placement-host-address
    
    * Update cmd/run.go
    
    Co-authored-by: Charlie Stanley <charlie.stanley@microsoft.com>
    
    Co-authored-by: Charlie Stanley <charlie.stanley@microsoft.com>

--- cmd/run.go
+++ cmd/run.go
@@ -78,7 +78,7 @@ var RunCmd = &cobra.Command{
 			LogLevel:           logLevel,
 			MaxConcurrency:     maxConcurrency,
 			Protocol:           protocol,
-			PlacementHost:      viper.GetString("placement-host-address"),
+			PlacementHostAddr:  viper.GetString("placement-host-address"),
 			ComponentsPath:     componentsPath,
 			AppSSL:             appSSL,
 			MetricsPort:        metricsPort,
@@ -276,7 +276,7 @@ func init() {
 	RunCmd.Flags().IntVarP(&maxConcurrency, "app-max-concurrency", "", -1, "The concurrency level of the application, otherwise is unlimited")
 	RunCmd.Flags().StringVarP(&protocol, "app-protocol", "P", "http", "The protocol (gRPC or HTTP) Dapr uses to talk to the application")
 	RunCmd.Flags().StringVarP(&componentsPath, "components-path", "d", standalone.DefaultComponentsDirPath(), "The path for components directory")
-	RunCmd.Flags().String("placement-host-address", "localhost", "The host on which the placement service resides")
+	RunCmd.Flags().String("placement-host-address", "localhost", "The address of the placement service. Format is either <hostname> for default port or <hostname>:<port> for custom port")
 	RunCmd.Flags().BoolVar(&appSSL, "app-ssl", false, "Enable https when Dapr invokes the application")
 	RunCmd.Flags().IntVarP(&metricsPort, "metrics-port", "M", -1, "The port of metrics on dapr")
 	RunCmd.Flags().BoolP("help", "h", false, "Print this help message")
