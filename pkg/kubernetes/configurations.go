9e7b9cbd2adcf3bebbe4bad11ab97929a7888983
Phil Kedy <phil.kedy@gmail.com>
2021-07-14 18:03:55 -0400

    Tweaks per PR review

--- pkg/kubernetes/configurations.go
+++ pkg/kubernetes/configurations.go
@@ -42,10 +42,9 @@ func PrintConfigurations(name, outputFormat string) error {
 
 		list, err := client.ConfigurationV1alpha1().Configurations(meta_v1.NamespaceAll).List(meta_v1.ListOptions{})
 		if err != nil {
-			// This means that the Dapr Configurations CRD is not installed and there
-			// is are therefore no configuration items.
-			if serr, ok := err.(*apierrors.StatusError); ok &&
-				serr.ErrStatus.Reason == meta_v1.StatusReasonNotFound {
+			// This means that the Dapr Configurations CRD is not installed and
+			// therefore no configuration items exist.
+			if apierrors.IsNotFound(err) {
 				list = &v1alpha1.ConfigurationList{
 					Items: []v1alpha1.Configuration{},
 				}
