6bbffc728c89e80febcb7f21dbb02995c0ac6d18
Andy Le <anhldbk@gmail.com>
2021-11-11 09:12:58 +0700

    version: simplify tests on bad addr

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -115,8 +114,7 @@ func TestGetVersionsGithub(t *testing.T) {
 	t.Run("error on bad addr", func(t *testing.T) {
 		version, err := GetLatestReleaseGithub("http://a.super.non.existant.domain/")
 		assert.Equal(t, "", version)
-		msg := fmt.Sprint(err)
-		assert.True(t, strings.Contains(msg, "no such host") || strings.Contains(msg, "i/o timeout"))
+		assert.Error(t, err)
 	})
 
 	s.Shutdown(context.Background())
