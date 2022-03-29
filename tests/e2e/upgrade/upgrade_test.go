a4afc090b4b58697f74c7faa2dd747e6c599b3fb
Amulya Varote <amulyavarote@microsoft.com>
2022-03-28 17:24:12 -0700

    Reverted dashboard version
    Signed-off-by: Amulya Varote <amulyavarote@microsoft.com>

--- tests/e2e/upgrade/upgrade_test.go
+++ tests/e2e/upgrade/upgrade_test.go
@@ -65,7 +65,7 @@ var supportedUpgradePaths = []upgradePath{
 	{
 		previous: common.VersionDetails{
 			RuntimeVersion:      "1.7.0-rc.1",
-			DashboardVersion:    "0.10.0-rc.1",
+			DashboardVersion:    "0.9.0",
 			ClusterRoles:        []string{"dapr-operator-admin", "dashboard-reader"},
 			ClusterRoleBindings: []string{"dapr-operator", "dapr-role-tokenreview-binding", "dashboard-reader-global"},
 			CustomResourceDefs:  []string{"components.dapr.io", "configurations.dapr.io", "subscriptions.dapr.io"},
