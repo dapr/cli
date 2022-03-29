4d5f3ed5373058114cd8b73ebfac59a56000adf3
Amulya Varote <amulyavarote@microsoft.com>
2022-03-28 17:52:25 -0700

    Reverted dashboard version
    Signed-off-by: Amulya Varote <amulyavarote@microsoft.com>

--- tests/e2e/upgrade/upgrade_test.go
+++ tests/e2e/upgrade/upgrade_test.go
@@ -61,23 +61,23 @@ var supportedUpgradePaths = []upgradePath{
 			CustomResourceDefs:  []string{"components.dapr.io", "configurations.dapr.io", "subscriptions.dapr.io"},
 		},
 	},
-	// test downgrade
 	{
 		previous: common.VersionDetails{
-			RuntimeVersion:      "1.7.0-rc.1",
-			DashboardVersion:    "0.9.0",
-			ClusterRoles:        []string{"dapr-operator-admin", "dashboard-reader"},
-			ClusterRoleBindings: []string{"dapr-operator", "dapr-role-tokenreview-binding", "dashboard-reader-global"},
-			CustomResourceDefs:  []string{"components.dapr.io", "configurations.dapr.io", "subscriptions.dapr.io"},
-		},
-		next: common.VersionDetails{
 			RuntimeVersion:      "1.6.0",
 			DashboardVersion:    "0.9.0",
 			ClusterRoles:        []string{"dapr-operator-admin", "dashboard-reader"},
 			ClusterRoleBindings: []string{"dapr-operator", "dapr-role-tokenreview-binding", "dashboard-reader-global"},
 			CustomResourceDefs:  []string{"components.dapr.io", "configurations.dapr.io", "subscriptions.dapr.io"},
 		},
+		next: common.VersionDetails{
+			RuntimeVersion:      "1.7.0-rc.1",
+			DashboardVersion:    "0.10.0-rc.1",
+			ClusterRoles:        []string{"dapr-operator-admin", "dashboard-reader"},
+			ClusterRoleBindings: []string{"dapr-operator", "dapr-role-tokenreview-binding", "dashboard-reader-global"},
+			CustomResourceDefs:  []string{"components.dapr.io", "configurations.dapr.io", "subscriptions.dapr.io"},
+		},
 	},
+	// test downgrade
 	{
 		previous: common.VersionDetails{
 			RuntimeVersion:      "1.6.0",
