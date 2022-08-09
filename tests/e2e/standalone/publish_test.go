a56c956fe45b194b538f328266292c60c365e775
Shubham Sharma <shubhash@microsoft.com>
2022-08-09 15:17:11 +0530

    Add pubsub tests
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- tests/e2e/standalone/publish_test.go
+++ tests/e2e/standalone/publish_test.go
@@ -86,67 +86,67 @@ func TestStandalonePublish(t *testing.T) {
 				assert.Equal(t, map[string]interface{}{"dapr": "is_great"}, event.Data)
 			})
 
-			// t.Run(fmt.Sprintf("publish cloudevent from file with socket %s", path), func(t *testing.T) {
-			// 	output, err := spawn.Command(daprPath, "publish", "--publish-app-id", "pub_e2e", "--unix-domain-socket", path, "--pubsub", "pubsub", "--topic", "sample", "--data-file", "../testdata/cloudevent.json")
-			// 	t.Log(output)
-			// 	assert.NoError(t, err, "unable to publish from --data-file")
-			// 	assert.Contains(t, output, "Event published successfully")
+			t.Run(fmt.Sprintf("publish cloudevent from file with socket %s", path), func(t *testing.T) {
+				output, err := cmdPublish("pub_e2e", "pubsub", "sample", path, "--data-file", "../testdata/cloudevent.json")
+				t.Log(output)
+				assert.NoError(t, err, "unable to publish from --data-file")
+				assert.Contains(t, output, "Event published successfully")
 
-			// 	event := <-events
-			// 	assert.Equal(t, &common.TopicEvent{
-			// 		ID:              "3cc97064-edd1-49f4-b911-c959a7370e68",
-			// 		Source:          "e2e_test",
-			// 		SpecVersion:     "1.0",
-			// 		Type:            "test.v1",
-			// 		DataContentType: "application/json",
-			// 		Subject:         "e2e_subject",
-			// 		PubsubName:      "pubsub",
-			// 		Topic:           "sample",
-			// 		Data:            map[string]interface{}{"dapr": "is_great"},
-			// 		RawData:         []byte(`{"dapr":"is_great"}`),
-			// 	}, event)
-			// })
+				event := <-events
+				assert.Equal(t, &common.TopicEvent{
+					ID:              "3cc97064-edd1-49f4-b911-c959a7370e68",
+					Source:          "e2e_test",
+					SpecVersion:     "1.0",
+					Type:            "test.v1",
+					DataContentType: "application/json",
+					Subject:         "e2e_subject",
+					PubsubName:      "pubsub",
+					Topic:           "sample",
+					Data:            map[string]interface{}{"dapr": "is_great"},
+					RawData:         []byte(`{"dapr":"is_great"}`),
+				}, event)
+			})
 
-			// t.Run(fmt.Sprintf("publish from string with socket %s", path), func(t *testing.T) {
-			// 	output, err := spawn.Command(daprPath, "publish", "--publish-app-id", "pub_e2e", "--unix-domain-socket", path, "--pubsub", "pubsub", "--topic", "sample", "--data", "{\"cli\": \"is_working\"}")
-			// 	t.Log(output)
-			// 	assert.NoError(t, err, "unable to publish from --data")
-			// 	assert.Contains(t, output, "Event published successfully")
+			t.Run(fmt.Sprintf("publish from string with socket %s", path), func(t *testing.T) {
+				output, err := cmdPublish("pub_e2e", "pubsub", "sample", path, "--data", "{\"cli\": \"is_working\"}")
+				t.Log(output)
+				assert.NoError(t, err, "unable to publish from --data")
+				assert.Contains(t, output, "Event published successfully")
 
-			// 	event := <-events
-			// 	assert.Equal(t, map[string]interface{}{"cli": "is_working"}, event.Data)
-			// })
+				event := <-events
+				assert.Equal(t, map[string]interface{}{"cli": "is_working"}, event.Data)
+			})
 
-			// t.Run(fmt.Sprintf("publish from non-existent file fails with socket %s", path), func(t *testing.T) {
-			// 	output, err := spawn.Command(daprPath, "publish", "--publish-app-id", "pub_e2e", "--unix-domain-socket", path, "--pubsub", "pubsub", "--topic", "sample", "--data-file", "a/file/that/does/not/exist")
-			// 	t.Log(output)
-			// 	assert.Contains(t, output, "Error reading payload from 'a/file/that/does/not/exist'. Error: ")
-			// 	assert.Error(t, err, "a non-existent --data-file should fail with error")
-			// })
+			t.Run(fmt.Sprintf("publish from non-existent file fails with socket %s", path), func(t *testing.T) {
+				output, err := cmdPublish("pub_e2e", "pubsub", "sample", path, "--data-file", "a/file/that/does/not/exist")
+				t.Log(output)
+				assert.Contains(t, output, "Error reading payload from 'a/file/that/does/not/exist'. Error: ")
+				assert.Error(t, err, "a non-existent --data-file should fail with error")
+			})
 
-			// t.Run(fmt.Sprintf("publish only one of data and data-file with socket %s", path), func(t *testing.T) {
-			// 	output, err := spawn.Command(daprPath, "publish", "--publish-app-id", "pub_e2e", "--unix-domain-socket", path, "--pubsub", "pubsub", "--topic", "sample", "--data-file", "../testdata/message.json", "--data", "{\"cli\": \"is_working\"}")
-			// 	t.Log(output)
-			// 	assert.Error(t, err, "--data and --data-file should not be allowed together")
-			// 	assert.Contains(t, output, "Only one of --data and --data-file allowed in the same publish command")
-			// })
+			t.Run(fmt.Sprintf("publish only one of data and data-file with socket %s", path), func(t *testing.T) {
+				output, err := cmdPublish("pub_e2e", "pubsub", "sample", path, "--data-file", "../testdata/message.json", "--data", "{\"cli\": \"is_working\"}")
+				t.Log(output)
+				assert.Error(t, err, "--data and --data-file should not be allowed together")
+				assert.Contains(t, output, "Only one of --data and --data-file allowed in the same publish command")
+			})
 
-			// t.Run(fmt.Sprintf("publish with invalid metadata fails"), func(t *testing.T) {
-			// 	output, err := spawn.Command(daprPath, "publish", "--publish-app-id", "pub_e2e", "--unix-domain-socket", path, "--pubsub", "pubsub", "--topic", "raw-sample", "--data", "{\"cli\": \"is_working\"}", "--metadata", "not a valid JSON")
-			// 	t.Log(output)
-			// 	assert.Error(t, err, "invalid metadata should fail")
-			// 	assert.Contains(t, output, "Error parsing metadata as JSON")
-			// })
+			t.Run("publish with invalid metadata fails", func(t *testing.T) {
+				output, err := cmdPublish("pub_e2e", "pubsub", "raw-sample", path, "--data", "{\"cli\": \"is_working\"}", "--metadata", "not a valid JSON")
+				t.Log(output)
+				assert.Error(t, err, "invalid metadata should fail")
+				assert.Contains(t, output, "Error parsing metadata as JSON")
+			})
 
-			// t.Run(fmt.Sprintf("publish message without cloud event using metadata with socket"), func(t *testing.T) {
-			// 	output, err := spawn.Command(daprPath, "publish", "--publish-app-id", "pub_e2e", "--unix-domain-socket", path, "--pubsub", "pubsub", "--topic", "raw-sample", "--data", "{\"cli\": \"is_working\"}", "--metadata", "{\"rawPayload\": \"true\"}")
-			// 	t.Log(output)
-			// 	assert.NoError(t, err, "unable to publish with rawPayload --metadata")
-			// 	assert.Contains(t, output, "Event published successfully")
+			t.Run("publish message without cloud event using metadata with socket", func(t *testing.T) {
+				output, err := cmdPublish("pub_e2e", "pubsub", "raw-sample", path, "--data", "{\"cli\": \"is_working\"}", "--metadata", "{\"rawPayload\": \"true\"}")
+				t.Log(output)
+				assert.NoError(t, err, "unable to publish with rawPayload --metadata")
+				assert.Contains(t, output, "Event published successfully")
 
-			// 	event := <-events
-			// 	assert.Equal(t, []byte("{\"cli\": \"is_working\"}"), event.Data)
-			// })
+				event := <-events
+				assert.Equal(t, []byte("{\"cli\": \"is_working\"}"), event.Data)
+			})
 
 			output, err := cmdStop("pub_e2e")
 			t.Log(output)
