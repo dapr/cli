20780f15820e73b860681a904e773a0788c1421a
Pravin Pushkar <ppushkar@microsoft.com>
2023-01-04 15:22:29 +0530

    UT refactor
    Signed-off-by: Pravin Pushkar <ppushkar@microsoft.com>

--- pkg/standalone/runfileconfig/run_file_config_parser_test.go
+++ pkg/standalone/runfileconfig/run_file_config_parser_test.go
@@ -14,6 +14,7 @@ limitations under the License.
 package runfileconfig
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -27,104 +28,99 @@ var (
 	invalidRunFilePath2 = filepath.Join("..", "testdata", "runfileconfig", "test_run_config_empty_app_dir.yaml")
 )
 
-func TestRunConfigParser(t *testing.T) {
+func TestRunConfigFile(t *testing.T) {
 	tearDownFn := testSetup(t)
 	defer tearDownFn(t)
 
-	appsRunConfig := RunFileConfig{}
-	err := appsRunConfig.parseAppsConfig(validRunFilePath)
+	t.Run("test run config parser", func(t *testing.T) {
+		appsRunConfig := RunFileConfig{}
+		err := appsRunConfig.parseAppsConfig(validRunFilePath)
 
-	assert.Nil(t, err)
-	assert.Equal(t, 2, len(appsRunConfig.Apps))
+		assert.Nil(t, err)
+		assert.Equal(t, 2, len(appsRunConfig.Apps))
 
-	assert.Equal(t, 1, appsRunConfig.Version)
-	assert.NotEmpty(t, appsRunConfig.Common.ResourcesPath)
-	assert.NotEmpty(t, appsRunConfig.Common.Env)
+		assert.Equal(t, 1, appsRunConfig.Version)
+		assert.NotEmpty(t, appsRunConfig.Common.ResourcesPath)
+		assert.NotEmpty(t, appsRunConfig.Common.Env)
 
-	firstAppConfig := appsRunConfig.Apps[0]
-	secondAppConfig := appsRunConfig.Apps[1]
-	assert.Equal(t, "", firstAppConfig.AppID)
-	assert.Equal(t, "GRPC", secondAppConfig.AppProtocol)
-	assert.Equal(t, 8080, firstAppConfig.AppPort)
-	assert.Equal(t, "", firstAppConfig.UnixDomainSocket)
-}
+		firstAppConfig := appsRunConfig.Apps[0]
+		secondAppConfig := appsRunConfig.Apps[1]
+		assert.Equal(t, "", firstAppConfig.AppID)
+		assert.Equal(t, "GRPC", secondAppConfig.AppProtocol)
+		assert.Equal(t, 8080, firstAppConfig.AppPort)
+		assert.Equal(t, "", firstAppConfig.UnixDomainSocket)
+	})
 
-func TestValidateRunConfig(t *testing.T) {
-	tearDownFn := testSetup(t)
-	defer tearDownFn(t)
+	t.Run("test run GetApps", func(t *testing.T) {
+		config := RunFileConfig{}
 
-	testcases := []struct {
-		name        string
-		input       string
-		expectedErr bool
-	}{
-		{
-			name:        "valid run config",
-			input:       validRunFilePath,
-			expectedErr: false,
-		},
-		{
-			name:        "invalid run config - empty app dir path",
-			input:       invalidRunFilePath2,
-			expectedErr: true,
-		},
-		{
-			name:        "invalid run config - invalid path",
-			input:       invalidRunFilePath1,
-			expectedErr: true,
-		},
-	}
+		apps, err := config.GetApps(validRunFilePath)
+		assert.Nil(t, err)
+		assert.Equal(t, 2, len(apps))
+		assert.Equal(t, "webapp", apps[0].AppID)
+		assert.Equal(t, "backend", apps[1].AppID)
+		assert.Equal(t, "HTTP", apps[0].AppProtocol)
+		assert.Equal(t, "GRPC", apps[1].AppProtocol)
+		assert.Equal(t, 8080, apps[0].AppPort)
+		assert.Equal(t, 3000, apps[1].AppPort)
+		assert.Equal(t, 1, apps[0].AppHealthTimeout)
+		assert.Equal(t, 10, apps[1].AppHealthTimeout)
+		assert.Equal(t, "", apps[0].UnixDomainSocket)
+		assert.Equal(t, "/tmp/test-socket", apps[1].UnixDomainSocket)
 
-	for _, tc := range testcases {
-		t.Run(tc.name, func(t *testing.T) {
-			config := RunFileConfig{}
-			config.parseAppsConfig(tc.input)
-			actualErr := config.validateRunConfig(tc.input)
-			assert.Equal(t, tc.expectedErr, actualErr != nil)
-		})
-	}
-}
+		// test resources_path and config_path after precedence order logic.
+		assert.Equal(t, filepath.Join(apps[0].AppDirPath, "resources"), apps[0].ResourcesPath)
+		assert.Equal(t, filepath.Join(apps[1].AppDirPath, ".dapr", "resources"), apps[1].ResourcesPath)
 
-func TestGetApps(t *testing.T) {
-	tearDownFn := testSetup(t)
-	defer tearDownFn(t)
+		assert.Equal(t, filepath.Join(apps[0].AppDirPath, "config.yaml"), apps[0].ConfigFile)
+		assert.Equal(t, filepath.Join(apps[1].AppDirPath, ".dapr", "config.yaml"), apps[1].ConfigFile)
 
-	config := RunFileConfig{}
+		// temporarily set apps[0].ResourcesPath to empty string to test it is getting picked from common section.
+		apps[0].ResourcesPath = ""
+		config.resolveResourcesAndConfigFilePaths()
+		assert.Equal(t, config.Common.ResourcesPath, apps[0].ResourcesPath)
+		assert.Equal(t, filepath.Join(apps[1].AppDirPath, ".dapr", "resources"), apps[1].ResourcesPath)
 
-	apps, err := config.GetApps(validRunFilePath)
-	assert.Nil(t, err)
-	assert.Equal(t, 2, len(apps))
-	assert.Equal(t, "webapp", apps[0].AppID)
-	assert.Equal(t, "backend", apps[1].AppID)
-	assert.Equal(t, "HTTP", apps[0].AppProtocol)
-	assert.Equal(t, "GRPC", apps[1].AppProtocol)
-	assert.Equal(t, 8080, apps[0].AppPort)
-	assert.Equal(t, 3000, apps[1].AppPort)
-	assert.Equal(t, 1, apps[0].AppHealthTimeout)
-	assert.Equal(t, 10, apps[1].AppHealthTimeout)
-	assert.Equal(t, "", apps[0].UnixDomainSocket)
-	assert.Equal(t, "/tmp/test-socket", apps[1].UnixDomainSocket)
+		// test merged envs from common and app sections.
+		assert.Equal(t, 2, len(apps[0].Env))
+		assert.Equal(t, 2, len(apps[1].Env))
+		assert.Equal(t, "DEBUG", apps[0].Env[0].Name)
+		assert.Equal(t, "false", apps[0].Env[0].Value)
+		assert.Equal(t, "DEBUG", apps[1].Env[0].Name)
+		assert.Equal(t, "true", apps[1].Env[0].Value)
+	})
+	t.Run("test validate config files", func(t *testing.T) {
+		testcases := []struct {
+			name        string
+			input       string
+			expectedErr bool
+		}{
+			{
+				name:        "valid run config",
+				input:       validRunFilePath,
+				expectedErr: false,
+			},
+			{
+				name:        "invalid run config - empty app dir path",
+				input:       invalidRunFilePath2,
+				expectedErr: true,
+			},
+			{
+				name:        "invalid run config - invalid path",
+				input:       invalidRunFilePath1,
+				expectedErr: true,
+			},
+		}
 
-	// test resources_path and config_path after precedence order logic.
-	assert.Equal(t, filepath.Join(apps[0].AppDirPath, "resources"), apps[0].ResourcesPath)
-	assert.Equal(t, filepath.Join(apps[1].AppDirPath, ".dapr", "resources"), apps[1].ResourcesPath)
-
-	assert.Equal(t, filepath.Join(apps[0].AppDirPath, "config.yaml"), apps[0].ConfigFile)
-	assert.Equal(t, filepath.Join(apps[1].AppDirPath, ".dapr", "config.yaml"), apps[1].ConfigFile)
-
-	// temporarily set apps[0].ResourcesPath to empty string to test it is getting picked from common section.
-	apps[0].ResourcesPath = ""
-	config.resolveResourcesAndConfigFilePaths()
-	assert.Equal(t, config.Common.ResourcesPath, apps[0].ResourcesPath)
-	assert.Equal(t, filepath.Join(apps[1].AppDirPath, ".dapr", "resources"), apps[1].ResourcesPath)
-
-	// test merged envs from common and app sections.
-	assert.Equal(t, 2, len(apps[0].Env))
-	assert.Equal(t, 2, len(apps[1].Env))
-	assert.Equal(t, "DEBUG", apps[0].Env[0].Name)
-	assert.Equal(t, "false", apps[0].Env[0].Value)
-	assert.Equal(t, "DEBUG", apps[1].Env[0].Name)
-	assert.Equal(t, "true", apps[1].Env[0].Value)
+		for _, tc := range testcases {
+			t.Run(tc.name, func(t *testing.T) {
+				config := RunFileConfig{}
+				config.parseAppsConfig(tc.input)
+				actualErr := config.validateRunConfig(tc.input)
+				assert.Equal(t, tc.expectedErr, actualErr != nil)
+			})
+		}
+	})
 }
 
 func TestGetBasePathFromAbsPath(t *testing.T) {
@@ -182,32 +178,54 @@ func testSetup(t *testing.T) func(t *testing.T) {
 	webappAppResourcesPath := filepath.Join(baseDir, "webapp", "resources")
 	webAppAppConfigPath := filepath.Join(baseDir, "webapp", "config.yaml")
 
-	var err error
-	err = os.MkdirAll(commonResourcesPath, 0o777)
+	err := createDirPath(commonResourcesPath, backendAppResourcesPath, webappAppResourcesPath)
 	assert.Nil(t, err)
 
-	err = os.MkdirAll(backendAppResourcesPath, 0o777)
-	assert.Nil(t, err)
-
-	backendConfigFile, err := os.Create(backendAppConfigPath)
-	assert.Nil(t, err)
-	err = backendConfigFile.Close()
-	assert.Nil(t, err)
-
-	err = os.MkdirAll(webappAppResourcesPath, 0o777)
-	assert.Nil(t, err)
-
-	webAppConfigFile, err := os.Create(webAppAppConfigPath)
-	assert.Nil(t, err)
-	err = webAppConfigFile.Close()
+	err = createFile(webAppAppConfigPath, backendAppConfigPath)
 	assert.Nil(t, err)
 
 	return func(t *testing.T) {
-		err := os.RemoveAll(filepath.Join(baseDir, "app"))
-		assert.Nil(t, err)
-		err = os.RemoveAll(filepath.Join(baseDir, "backend"))
-		assert.Nil(t, err)
-		err = os.RemoveAll(filepath.Join(baseDir, "webapp"))
+		err := removeAllDirPaths(filepath.Join(baseDir, "app"), filepath.Join(baseDir, "backend"), filepath.Join(baseDir, "webapp"))
 		assert.Nil(t, err)
 	}
+}
+
+// Helper function to create slice of directory paths.
+func createDirPath(dirPaths ...string) error {
+	for _, path := range dirPaths {
+		if path != "" {
+			if err := os.MkdirAll(path, 0o777); err != nil {
+				return fmt.Errorf("error in creating the directory path %s: %w", path, err)
+			}
+		}
+	}
+	return nil
+}
+
+// Helper function to create slice of file paths.
+func createFile(filePaths ...string) error {
+	for _, path := range filePaths {
+		if path != "" {
+			file, err := os.Create(path)
+			if err != nil {
+				return fmt.Errorf("error in creating the file %s: %w", path, err)
+			}
+			if err := file.Close(); err != nil {
+				return fmt.Errorf("error in closing the file %s: %w", path, err)
+			}
+		}
+	}
+	return nil
+}
+
+// Helper function to remove slice of directory paths.
+func removeAllDirPaths(dirPaths ...string) error {
+	for _, path := range dirPaths {
+		if path != "" {
+			if err := os.RemoveAll(path); err != nil {
+				return fmt.Errorf("error in removing the directory path %s: %w", path, err)
+			}
+		}
+	}
+	return nil
 }
