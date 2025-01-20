3acfa7d7e455b84b413153872343d4e6f0c488b6
Mike Nguyen <hey@mike.ee>
2025-01-20 22:18:31 +0000

    fix: population of the schedulerhostaddress in self-hosted mode (#1475)
    * fix: population of the schedulerhostaddress in self-hosted mode
    
    The scheduler host address is pre-populated when using the self-hosted mode multi-app run similarly to the single app run.
    Kubernetes multi-app run is not affected and you will still need to specify a scheduler host address.
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    * chore: lint
    
    Signed-off-by: mikeee <hey@mike.ee>
    
    ---------
    
    Signed-off-by: mikeee <hey@mike.ee>
    Co-authored-by: Yaron Schneider <schneider.yaron@live.com>

---
+++ cmd/run_test.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateSchedulerHostAddress(t *testing.T) {
+	t.Run("test scheduler host address - v1.14.0-rc.0", func(t *testing.T) {
+		address := validateSchedulerHostAddress("1.14.0-rc.0", "")
+		assert.Equal(t, "", address)
+	})
+
+	t.Run("test scheduler host address - v1.15.0-rc.0", func(t *testing.T) {
+		address := validateSchedulerHostAddress("1.15.0", "")
+		assert.Equal(t, "localhost:50006", address)
+	})
+}
