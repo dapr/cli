374b61d696b4d5cca6ba6ab7c098a395660a0d6f
yaron2 <schneider.yaron@live.com>
2025-01-21 16:44:16 -0800

    use non-deprecated flags in List operation
    Signed-off-by: yaron2 <schneider.yaron@live.com>

--- pkg/standalone/list.go
+++ pkg/standalone/list.go
@@ -105,9 +105,9 @@ func List() ([]ListOutput, error) {
 				enableMetrics = true
 			}
 
-			maxRequestBodySize := getIntArg(argumentsMap, "--dapr-http-max-request-size", runtime.DefaultMaxRequestBodySize)
+			maxRequestBodySize := getIntArg(argumentsMap, "http-max-request-size", runtime.DefaultMaxRequestBodySize)
 
-			httpReadBufferSize := getIntArg(argumentsMap, "--dapr-http-read-buffer-size", runtime.DefaultReadBufferSize)
+			httpReadBufferSize := getIntArg(argumentsMap, "http-read-buffer-size", runtime.DefaultReadBufferSize)
 
 			appID := argumentsMap["--app-id"]
 			appCmd := ""
