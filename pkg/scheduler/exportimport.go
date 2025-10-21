41d1c992f27dc52977d04979b88716102b9e442a
joshvanl <me@joshvanl.dev>
2025-10-21 10:12:11 +0100

    Review comments
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- pkg/scheduler/exportimport.go
+++ pkg/scheduler/exportimport.go
@@ -118,7 +118,6 @@ func Import(ctx context.Context, opts ExportImportOptions) error {
 	ops := make([]clientv3.Op, 0, len(in.Jobs)+len(in.Counters))
 
 	for key, b := range in.Jobs {
-		// Optional: verify bytes are valid before writing
 		var j stored.Job
 		if err := proto.Unmarshal(b, &j); err != nil {
 			return fmt.Errorf("unmarshal job %q: %w", key, err)
@@ -134,19 +133,21 @@ func Import(ctx context.Context, opts ExportImportOptions) error {
 		ops = append(ops, clientv3.OpPut(key, string(b)))
 	}
 
+	var end int
 	for i := 0; i < len(ops); i += 128 {
 		txn := client.Txn(ctx)
-		end := i + 128
+		end = i + 128
 		if end > len(ops) {
 			end = len(ops)
 		}
 		txn.Then(ops[i:end]...)
 		if _, err := txn.Commit(); err != nil {
+			print.FailureStatusEvent(os.Stderr, "Incomplete import with %d items.", end)
 			return fmt.Errorf("commit transaction: %w", err)
 		}
-
-		print.InfoStatusEvent(os.Stdout, "Imported %d items.", end)
 	}
+
+	print.InfoStatusEvent(os.Stdout, "Imported %d items.", end)
 
 	return nil
 }
--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -148,7 +148,7 @@ func parseJobKey(key string) (*jobKey, error) {
 	}
 
 	switch split[0] {
-	case "app":
+	case FilterApp:
 		if len(split) != 3 {
 			return nil, fmt.Errorf("expecting job key to be in format 'app/{app ID}/{job name}', got '%s'", key)
 		}
@@ -157,7 +157,7 @@ func parseJobKey(key string) (*jobKey, error) {
 			name:  split[2],
 		}, nil
 
-	case "actor":
+	case FilterActor:
 		if len(split) != 4 {
 			return nil, fmt.Errorf("expecting actor reminder key to be in format 'actor/{actor type}/{actor id}/{name}', got '%s'", key)
 		}
@@ -167,9 +167,9 @@ func parseJobKey(key string) (*jobKey, error) {
 			name:      split[3],
 		}, nil
 
-	case "workflow":
+	case FilterWorkflow:
 		if len(split) != 4 {
-			return nil, fmt.Errorf("expecting worklow key to be in format 'workflow/{app ID}/{instance ID}/{name}', got '%s'", key)
+			return nil, fmt.Errorf("expecting workflow key to be in format 'workflow/{app ID}/{instance ID}/{name}', got '%s'", key)
 		}
 		return &jobKey{
 			appID:      &split[1],
@@ -177,9 +177,9 @@ func parseJobKey(key string) (*jobKey, error) {
 			name:       split[3],
 		}, nil
 
-	case "activity":
+	case FilterActivity:
 		if len(split) != 3 {
-			return nil, fmt.Errorf("expecting activity key to be in format 'activity/{app ID}/{activity ID}/', got '%s'", key)
+			return nil, fmt.Errorf("expecting activity key to be in format 'activity/{app ID}/{activity ID}', got '%s'", key)
 		}
 		return &jobKey{
 			appID:    &split[1],
@@ -188,7 +188,7 @@ func parseJobKey(key string) (*jobKey, error) {
 		}, nil
 
 	default:
-		return nil, fmt.Errorf("unsupported job type '%s', accepts 'app', 'actor', or 'workflow'", split[0])
+		return nil, fmt.Errorf("unsupported job type '%s', accepts 'app', 'actor', 'workflow', or 'activity'", split[0])
 	}
 }
 
