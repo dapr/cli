6ec425cdb14103ddce8cb5c8f84faa51e75faf4b
joshvanl <me@joshvanl.dev>
2026-03-13 00:30:09 +0000

    Fix workflow list MongoDB fallback
    The CLI's direct MongoDB fallback (used when gRPC ListInstanceIDs is
    unavailable) searched for a "key" field, but the Dapr MongoDB state
    store stores document keys in the "_id" field. This caused
    `dapr workflow list -c 'mongodb://...'` to always return empty results.
    
    Signed-off-by: joshvanl <me@joshvanl.dev>

---
+++ pkg/workflow/db/mongo_test.go
@@ -0,0 +1,77 @@
+/*
+Copyright 2026 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package db
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+)
+
+func TestListMongo(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+
+	mt.Run("returns keys from _id field", func(mt *mtest.T) {
+		key1 := "myapp||dapr.internal.default.myapp.workflow||instance-1||metadata"
+		key2 := "myapp||dapr.internal.default.myapp.workflow||instance-2||metadata"
+
+		mt.AddMockResponses(mtest.CreateCursorResponse(1, "daprStore.daprCollection", mtest.FirstBatch,
+			bson.D{{Key: "_id", Value: key1}},
+		), mtest.CreateCursorResponse(0, "daprStore.daprCollection", mtest.NextBatch,
+			bson.D{{Key: "_id", Value: key2}},
+		))
+
+		keys, err := ListMongo(mt.Context(), mt.DB, "daprCollection", ListOptions{
+			Namespace: "default",
+			AppID:     "myapp",
+		})
+		require.NoError(t, err)
+		assert.Equal(t, []string{key1, key2}, keys)
+	})
+
+	mt.Run("returns nil when no documents match", func(mt *mtest.T) {
+		mt.AddMockResponses(mtest.CreateCursorResponse(0, "daprStore.daprCollection", mtest.FirstBatch))
+
+		keys, err := ListMongo(mt.Context(), mt.DB, "daprCollection", ListOptions{
+			Namespace: "default",
+			AppID:     "myapp",
+		})
+		require.NoError(t, err)
+		assert.Nil(t, keys)
+	})
+
+	mt.Run("filter uses _id field with correct regex", func(mt *mtest.T) {
+		mt.AddMockResponses(mtest.CreateCursorResponse(0, "daprStore.daprCollection", mtest.FirstBatch))
+
+		_, err := ListMongo(mt.Context(), mt.DB, "daprCollection", ListOptions{
+			Namespace: "default",
+			AppID:     "myapp",
+		})
+		require.NoError(t, err)
+
+		// Verify the find command was sent with _id filter.
+		cmd := mt.GetStartedEvent().Command
+		filterVal := cmd.Lookup("filter")
+		filterDoc := filterVal.Document()
+
+		// The filter should contain an "_id" field, not "key".
+		_, err = filterDoc.LookupErr("_id")
+		assert.NoError(t, err, "filter should query on _id field")
+		_, err = filterDoc.LookupErr("key")
+		assert.Error(t, err, "filter should not query on key field")
+	})
+}
